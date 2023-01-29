ca517a3280e26b00c9c7264ef9dc62902098c454
Karim Arbia <karim.sasor@gmail.com>
2023-01-29 18:05:03 +0100

    v1 finished

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const configuration = new Configuration({
+  organization: "org-6Cta5L2FSblAYbFykVvgpZZN",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+// api
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 3000;
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+// app.get("/models", async (req, res) => {
+//   const response = await openai.listEngines();
+
+//   res.json({
+//     models: response.data.data,
+//   });
+// });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
